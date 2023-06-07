54dc1f8aafbe332bcc9d5a9cf89bfcbe5ada7eee
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-06-07 22:59:23 +0200

    add gallery task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
+const galleryItem = galleryItems
+  .map(
+    (item) =>
+      `<a class="gallery__item" href=${item.preview}>
+    <img class="gallery__image" src=${item.preview} alt=${item.description} />
+  </a>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", galleryItem);
 console.log(galleryItems);
